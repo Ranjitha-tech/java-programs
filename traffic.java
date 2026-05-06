c6f4a8862dcfff75b1053e55412f81631a996cbe
Your Name <your_email@example.com>
2026-05-06 17:06:31 +0530

    traffic signal

---
+++ traffic.java
@@ -0,0 +1,20 @@
+class traffic{
+    public static void main(String[] args)
+     {
+        {
+            String color="red";
+            if(color=="red")
+            {
+                System.out.println("Stop");
+            }
+            else if(color=="yellow")
+            {
+                System.out.println("Ready to go");
+            }
+            else if(color=="green")
+            {
+                System.out.println("go");
+            }
+        }
+    }
+}
