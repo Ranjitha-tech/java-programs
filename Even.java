084102f741ec43822143bebb0fa8d94e3ae43519
Your Name <your_email@example.com>
2026-05-06 16:48:58 +0530

    Even Num

---
+++ Even.java
@@ -0,0 +1,16 @@
+class Even{
+    public static void main(String[]args)
+    {
+        int n=7;
+
+        if(n%2==0)
+        {
+            System.out.println("Even number");
+        }
+        else
+        {
+            System.out.println("odd Number");
+        }
+    
+    }
+}
