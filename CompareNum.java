424504b23cd7c7ef8c6f7832bcdd5521944dea36
Your Name <your_email@example.com>
2026-05-06 17:21:07 +0530

    Number comparison

---
+++ CompareNum.java
@@ -0,0 +1,18 @@
+class CompareNum{
+    public static void main(String[]args)
+    {
+        int a=25;
+        int b=45;
+        if(a>b)
+        {
+            System.out.println(a+ " is greater than "+b);
+        }
+        else if(b>a)
+        {
+            System.out.println(b+ " is greater than "+a);
+        }
+        else{
+            System.out.println(a+" is equal to "+b);
+        }
+    }
+}
